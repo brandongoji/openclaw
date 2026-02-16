8bc7e469e48d09ecda12c4e5e1bf26d06cbbb0db
brandongoji <brandonlawes@gmail.com>
2026-02-16 12:07:51 -0500

    ckpt | 2026-02-16 12:08 EST | moonshine live fix pointer capture

--- ui/src/ui/views/chat.ts
+++ ui/src/ui/views/chat.ts
@@ -457,7 +457,6 @@ export function renderChat(props: ChatProps) {
                 void props.onMoonshinePTTStop?.();
               }}
               @pointercancel=${() => void props.onMoonshinePTTCancel?.()}
-              @lostpointercapture=${() => void props.onMoonshinePTTCancel?.()}
               @click=${(e: Event) => e.preventDefault()}
               title="Hold to talk with Moonshine local transcription"
             >
